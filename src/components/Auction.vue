da843a2e4c7688aec02bc141dcb9f49f8d732ee9
Spooore <erward.erward@gmail.com>
2019-02-03 20:28:14 +0100

    checkboxes

--- src/components/Auction.vue
+++ src/components/Auction.vue
@@ -16,8 +16,13 @@
                         {{item.title}}
                     </v-list-tile-content>
                     <v-list-tile-content
-                    class="font-weight-black red--text display-1 d-block text-xs-right">
-                    {{item.rrso}}
+                            class="font-weight-black red--text display-1 d-block text-xs-right">
+                        {{item.rrso}}
+                        <v-btn v-if="auction_stopped"
+                               fab dark color="red" small>
+                            <v-icon dark>done_outline</v-icon>
+                        </v-btn>
+
                     </v-list-tile-content>
                 </v-list-tile>
             </template>
@@ -36,6 +41,7 @@
 
 <script>
     import Vue from 'vue'
+
     export default {
 
         name: "Auction",
@@ -43,16 +49,16 @@
             return {
                 auction_stopped: false,
                 button_text: "Wstrzymaj licytację",
-                helper_items:[
-                    { header: 'Aktualny ranking' },
-                    { divider: true, inset: true },
-                    { divider: true, inset: true },
-                    { divider: true, inset: true },
-                    { divider: true, inset: true },
+                helper_items: [
+                    {header: 'Aktualny ranking'},
+                    {divider: true, inset: true},
+                    {divider: true, inset: true},
+                    {divider: true, inset: true},
+                    {divider: true, inset: true},
                 ],
                 bank_items: [
 
-                    {title: 'Alior   ', avatar: require('@/assets/LOGA-01.png'), rrso: "1.8%", rrso_int: 1.8 },
+                    {title: 'Alior   ', avatar: require('@/assets/LOGA-01.png'), rrso: "1.8%", rrso_int: 1.8},
                     {title: 'PKO BP  ', avatar: require('@/assets/LOGA-02.png'), rrso: '1,7%', rrso_int: 1.7},
                     {title: 'PKO SA  ', avatar: require('@/assets/LOGA-03.png'), rrso: '2,1%', rrso_int: 2.1},
                     {title: 'Milenium', avatar: require('@/assets/LOGA-04.png'), rrso: '2,2%', rrso_int: 2.2},
@@ -61,47 +67,43 @@
             }
         },
         methods: {
-            StopAuction()
-            {
-                this.auction_stopped=true;
+            StopAuction() {
+                this.auction_stopped = true;
             },
-            GetButtonText()
-            {
+            GetButtonText() {
                 return this.button_text;
             },
-            Time()
-            {
+            Time() {
                 var now = new Date();
-                var diff=3;
-                var countDownDate = new Date(now.getTime() + diff*60000);
+                var diff = 3;
+                var countDownDate = new Date(now.getTime() + diff * 60000);
 
                 var x = setInterval(() => {
                     var now = new Date().getTime();
                     var distance = countDownDate - now;
                     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                    if (/^\d$/.test(seconds))  {
-                        seconds = "0"+seconds;
+                    if (/^\d$/.test(seconds)) {
+                        seconds = "0" + seconds;
                     }
 
 
                     this.bank_items.forEach(bank => {
 
-                        let chance=Math.floor(Math.random()*10);
-                        if(chance<0.5)
-                        {
-                            bank.rrso_int=Math.random()*bank.rrso_int+0.1;
-                            bank.rrso_int=bank.rrso_int.toFixed(2);
-                            bank.rrso=bank.rrso_int+"%";
+                        let chance = Math.floor(Math.random() * 10);
+                        if (chance < 0.5) {
+                            bank.rrso_int = Math.random() * bank.rrso_int + 0.1;
+                            bank.rrso_int = bank.rrso_int.toFixed(2);
+                            bank.rrso = bank.rrso_int + "%";
                         }
 
                     });
 
-                    this.bank_items.sort((a,b)=> (a.rrso_int > b.rrso_int) ? 1 : ((b.rrso_int > a.rrso_int) ? -1 : 0));
-                    document.getElementById("demo").innerHTML = minutes + ":" + seconds ;
+                    this.bank_items.sort((a, b) => (a.rrso_int > b.rrso_int) ? 1 : ((b.rrso_int > a.rrso_int) ? -1 : 0));
+                    document.getElementById("demo").innerHTML = minutes + ":" + seconds;
 
-                    if (distance < 0 || this.auction_stopped===true) {
-                        this.button_text="Rozpocznij Rundę 2"
+                    if (distance < 0 || this.auction_stopped === true) {
+                        this.button_text = "Rozpocznij Rundę 2"
                         clearInterval(x);
                         document.getElementById("demo").innerHTML = "Koniec Czasu";
                     }
@@ -109,21 +111,20 @@
 
             },
         },
-        beforeMount(){
+        beforeMount() {
             this.Time();
         },
         computed: {
             items() {
-                    var output=[];
-                    let i=0;
-                    for (i=0;i<Math.max(this.bank_items.length,this.helper_items.length);i++)
-                    {
+                var output = [];
+                let i = 0;
+                for (i = 0; i < Math.max(this.bank_items.length, this.helper_items.length); i++) {
 
-                        output.push(this.helper_items[i]);
-                        console.log(this.bank_items[i].rrso_int);
-                        output.push(this.bank_items[i]);
-                    }
-                    return output;
+                    output.push(this.helper_items[i]);
+                    // console.log(this.bank_items[i].rrso_int);
+                    output.push(this.bank_items[i]);
+                }
+                return output;
             }
         }
     }
@@ -131,10 +132,11 @@
 
 <style scoped>
 
-    #Wstrzymaj{
+    #Wstrzymaj {
         margin-top: 70px;
     }
-    #demo{
+
+    #demo {
         margin-top: 5px;
     }
 
